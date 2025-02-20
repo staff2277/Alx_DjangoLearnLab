cb1fd0c9d46850f7089b5fd5b7ad03ccdc4e7a87
Mustapha Osman <wnfynn@gmail.com>
2025-02-20 16:56:27 +0000

    q2

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,15 +2,11 @@ from django.shortcuts import render
 from django.views.generic import DetailView
 from .models import Library, Book
 
-
-
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = "library_detail.html"
+    template_name = "relationship_app/library_detail.html"  # Ensure correct template path
     context_object_name = "library"
 
-
 def list_books(request):
-    books = Book.objects.select_related("author").all() 
-    return render(request, "list_books.html", {"books": books})
-
+    books = Book.objects.all()  # Changed to use Book.objects.all() as required
+    return render(request, "relationship_app/list_books.html", {"books": books})  # Fixed template path
