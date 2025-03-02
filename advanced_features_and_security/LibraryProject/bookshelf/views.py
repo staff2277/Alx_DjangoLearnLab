fcb1f518bb319ff85701db192039f49111be6de6
Mustapha Osman <wnfynn@gmail.com>
2025-03-02 17:59:23 +0000

    q3

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,41 +1,22 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render
 from django.contrib.auth.decorators import permission_required
-from .forms import BookSearchForm, ExampleForm
-from .models import Article, Book
 from django.db.models import Q
+from .models import Book
+from .forms import BookSearchForm
+from .forms import ExampleForm  # Explicit separate import
 
 @permission_required('bookshelf.can_view', raise_exception=True)
 def book_list(request):
-    books = Book.objects.all() 
-    form = BookSearchForm(request.GET or None)
-    if form.is_valid():
+    books = Book.objects.all()  
+    form = BookSearchForm(request.GET or None)  # Initialize search form
+    
+    if form.is_valid():  # Validate form input
         search_query = form.cleaned_data.get("search_query")
         if search_query:
             books = books.filter(Q(title__icontains=search_query) | Q(author__icontains=search_query))
 
-    return render(request, 'bookshelf/book_list.html', {'books': books})
+    return render(request, 'bookshelf/book_list.html', {'books': books, 'form': form})
 
 def example_form_view(request):
     form = ExampleForm()
     return render(request, 'bookshelf/example_form.html', {'form': form})
-
-@permission_required('bookshelf.can_view', raise_exception=True)
-def view_articles(request):
-    articles = Article.objects.all()
-    return render(request, 'articles/view.html', {'articles': articles})
-
-@permission_required('bookshelf.can_create', raise_exception=True)
-def create_article(request):
-    return render(request, 'articles/create.html')
-
-@permission_required('bookshelf.can_edit', raise_exception=True)
-def edit_article(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
-    return render(request, 'articles/edit.html', {'article': article})
-
-@permission_required('bookshelf.can_delete', raise_exception=True)
-def delete_article(request, article_id):
-    article = get_object_or_404(Article, id=article_id)
-    article.delete()
-    return render(request, 'articles/delete.html')
-
