6870cd6c4e60659fd72b44a545137a26f83e0d5a
Mustapha Osman <wnfynn@gmail.com>
2025-04-11 12:31:38 +0000

    urls.py changes

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,11 +18,11 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('profile/', views.profile, name='profile'),
     path('post/', PostListView.as_view(), name='post-list'),
-    path('post/new/', PostCreateView.as_view(), name='post-create'), 
+    path('post/new/', PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
     path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='post-comment'),
-    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit-comment'),
+    path('comment/<int:pk>/edit/', CommentUpdateView.as_view(), name='edit-comment'),  # This line
     path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='delete-comment'),
 ]
